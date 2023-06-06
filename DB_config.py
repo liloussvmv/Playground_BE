b40c0a0f3c438fd8fc45d52c0e8affb663e1c616
Oussvmv <xangrypro@gmail.com>
2023-06-06 13:29:58 +0200

    Trying to Deploy

--- DB_config.py
+++ DB_config.py
@@ -1,13 +1,7 @@
 db_config = {
     'user': 'root',
-    'password': '',
+    'password': 'CyberSec',
     'host': 'localhost',
-<<<<<<< HEAD
-    'port': '3308',
+    'port': '3306',
     'database': 'cybersec_playground'
 }
-=======
-    'port': '3306',
-    'database': 'cyber_play'
-}
->>>>>>> d1896f24e956233e0b46ba7bd5f465bc08306497
